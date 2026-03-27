78ca4de7e2e982ae8023d3119805bd1145424e80
Armel200 <demanouarmel@icloud.com>
2026-03-27 17:32:52 +0100

    first commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,18 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                        serif: ['Playfair Display', 'serif'],
+                    },
+                    colors: {
+                        brand: {
+                            purple: '#A855F7',
+                            lightPurple: '#F3E8FF',
+                            text: '#111827',
+                            muted: '#4B5563'
+                        }
+                    }
+                }
+            }
+        }
